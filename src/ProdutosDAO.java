ef9c28f50b563580e11781a70f63b6799bda7e34
Murilo da Costa Almeida de Jesus <murilo.costajesus@gmail.com>
2026-06-06 16:01:52 -0300

    Tela cadastroVIEW definida como principal

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -22,13 +22,33 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+  public void cadastrarProduto(ProdutosDTO produto){
+
+    String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+
+        conn = new conectaDAO().connectDB();
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null,
+                "Produto cadastrado com sucesso!");
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null,
+                "Erro ao cadastrar: " + e.getMessage());
+
     }
+
+}
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,8 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+         conn = DriverManager.getConnection(
+"jdbc:mysql://localhost/uc11?user=root&password=admin@1234");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
